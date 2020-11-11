69fd04b961c13620ea81bd8ddfed4281c2b85303
Stanislav <labushniak@gmail.com>
2020-11-11 19:59:43 +0300

    регистрация

---
+++ погружение/регистрация/register.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=marlin_users', 'root', 'root');
+$sql = 'SELECT * FROM users_table WHERE email = :email;';
+$statement = $pdo->prepare($sql);
+$statement->execute(['email' => $email]);
+
+$is_double = $statement->fetch(PDO::FETCH_ASSOC);
+
+if($is_double == false) {
+    $sql = 'INSERT INTO users_table (email, password) VALUES (:email, :password);';
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['email' => $email, 'password' => $password]);
+
+    $_SESSION['status'] = 'success';
+    $_SESSION['message'] = 'Эл. адрес будет вашим логином при авторизации';
+    header('Location: /auth/login.php');
+    exit;
+} else {
+    $_SESSION['status'] = 'danger';
+    $_SESSION['message'] = 'Такой эл. адрес уже есть в базе!';
+    header('Location: /auth/login.php');
+    exit;
+}
+?>
