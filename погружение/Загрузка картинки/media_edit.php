ca8389f012da903057aefbce693ac23729b8c0b2
Stanislav <labushniak@gmail.com>
2020-11-16 17:55:02 +0300

    Add files via upload
    изменил страницу переадресации на страницу профиля

--- погружение/Загрузка картинки/media_edit.php
+++ погружение/Загрузка картинки/media_edit.php
@@ -8,11 +8,6 @@ $user_id = $_GET['id'];
 $user = get_user_by_id($user_id);
 
 
-if ($avatar['name']==''){
-    set_flash_message ('danger', 'Не задана картинка!');
-    redirect_to('media.php?id=' . $user_id);
-} else {
-    set_avatar($user_id, $avatar);
-    set_flash_message ('success', 'Аватар обновлен!');
-    redirect_to('users.php');
-}
+set_avatar($user_id, $avatar);
+set_flash_message ('success', 'Картинка обновлена!');
+redirect_to('page_profile.php?id='.$user_id);
