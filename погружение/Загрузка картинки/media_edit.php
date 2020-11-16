9b672f040c10a94de843be97850020042a3d9974
Stanislav <labushniak@gmail.com>
2020-11-16 14:19:29 +0300

    Add files via upload

---
+++ погружение/Загрузка картинки/media_edit.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require "functions.php";
+
+$avatar = $_FILES['avatar'];
+$user_id = $_GET['id'];
+
+
+if ($avatar['name']==''){
+    set_flash_message ('danger', 'Не задана картинка!');
+    redirect_to('media.php?id=' . $user_id);
+} else {
+    set_avatar($user_id, $avatar);
+    set_flash_message ('success', 'Аватар обновлен!');
+    redirect_to('users.php');
+}
